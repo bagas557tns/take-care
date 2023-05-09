e0a8b9c68f9ad0e053a9452a14b47fc441f694b2
bagas557tns <bagas557@newschool.edu>
2023-05-09 07:39:04 -0400

    push 1 please work

---
+++ script.js
@@ -0,0 +1,21 @@
+// console.log('hello');
+
+// var width;
+
+// $("document").ready(function(){
+//     width = $(window).width();
+//     displayText();
+// });
+
+// $("#handle").draggable({
+//     grid: [50, 50],
+//     axis: "x",
+//     containment: "#container",
+//     zIndex: 100,
+//     drag: function(event, ui){
+//         ui.position.left = Math.min(Math.max( 50, ui.position.left ), width - 100);
+//         let x = ui.position.left + 12.5;
+
+//         $("#left").css("width", x);
+//     }
+// });
